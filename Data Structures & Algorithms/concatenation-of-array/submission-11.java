9a6053252638470b7c84b2e7b2ff13da20930cdf
Ng N Minh Anh <106756843+April-nnma@users.noreply.github.com>
2026-07-05 01:44:38 +0700

    Add: concatenation-of-array - submission-11

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-11.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n*2];
+
+        for(int i = 0; i<n; i ++){
+            int current = nums[i];
+             int indexFirst = i;
+             int indexSecond = n + i; 
+
+             ans[indexFirst] = current;
+             ans[indexSecond] = current; 
+        }
+        return ans;
+    }
+}    
