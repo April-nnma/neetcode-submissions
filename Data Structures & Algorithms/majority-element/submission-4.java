8bf1def2944e0709aa8e2c99b12c318b1b91bcdb
Ng N Minh Anh <106756843+April-nnma@users.noreply.github.com>
2026-07-05 23:23:15 +0700

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> frequency = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            int current = nums[i];
+            int count;
+            if(frequency.containsKey(current)){
+                int oldCount = frequency.get(current);
+                count = oldCount + 1;
+            }else{
+                count = 1;
+            }
+            frequency.put(current, count);
+            int n = nums.length/2;
+            if(count>n){
+                return current;
+            }
+        }
+        return -1;
+    }
+}
