5033271e45ed91aa7e90bb4456fe2f549a7213fd
Ng N Minh Anh <106756843+April-nnma@users.noreply.github.com>
2026-07-31 00:38:50 +0700

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < operations.length; i++){
+            String current = operations[i];
+
+            if(current.equals("C")){
+                stack.pop();
+            }else if(current.equals("D")){
+                int lastS = stack.peek();
+                int newS = lastS * 2;
+                stack.push(newS);
+            }else if(current.equals("+")){
+                int lastS = stack.pop(); //remove 
+                int getS = stack.peek();     //get
+                stack.push(lastS);      //undo remove
+                int newS = getS + lastS;
+                stack.push(newS);
+            }else{
+                int number = Integer.parseInt(current);
+                stack.push(number);
+            }    
+        }
+        int total = 0;
+        while(!stack.isEmpty()){
+            total += stack.pop();
+        }
+        return total;
+    }
+        
+}
