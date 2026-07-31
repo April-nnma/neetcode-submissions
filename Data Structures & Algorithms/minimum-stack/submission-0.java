6861f22db701b9a46436aeba831c1e4a0bf421e6
Ng N Minh Anh <106756843+April-nnma@users.noreply.github.com>
2026-07-31 23:22:17 +0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,39 @@
+class MinStack {
+    private Stack<Integer> mainStack;
+    private Stack<Integer> minStack;
+    public MinStack() {
+        mainStack = new Stack<>();
+        minStack = new Stack<>();
+        
+    }
+    
+    public void push(int val) {
+        mainStack.push(val);
+
+        if(minStack.isEmpty()){
+            minStack.push(val);
+        }else{
+            int minimum = minStack.peek();
+            if(val<minimum){
+                minStack.push(val);
+            }else{
+                minStack.push(minimum);
+            }
+        }
+        
+    }
+    
+    public void pop() {
+        mainStack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return mainStack.peek();
+        
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
